a69a9a673047cd57a43ac05d054554d138b01547
Pooja gour <poojagour231@gmail.com>
2025-04-01 13:35:28 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    int r=1;
+    for(int i=1;i<=n;i++)
+    r=r*2;
+    cout<<r<<endl;
+}

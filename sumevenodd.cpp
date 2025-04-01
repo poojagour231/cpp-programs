af18240a2431e28c351df9912e5ad0554f865344
Pooja gour <poojagour231@gmail.com>
2025-04-01 13:22:17 +0530

    Add files via upload

---
+++ sumevenodd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"even";
+    int sum=0;
+    for(int i=1;i<=100;i++)
+        if(i%2==0)
+        sum+=i;
+        cout<<sum<<endl;
+        cout<<"odd";
+         sum=0;
+        for(int i=1;i<=100;i++)
+        if(i%2!=0)
+        sum+=i;
+        cout<<sum<<endl;
+
+
+    
+    
+}

e82b13372db5f535e70989bbd914084501070d07
Pooja gour <poojagour231@gmail.com>
2025-04-01 13:47:06 +0530

    Add files via upload

---
+++ maxmacro.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define max2(a,b)
+int main(){
+    int a,b;
+    cout<<"enter a number a,b";
+    cin>>a>>b;
+  int largest=max2(a,b);
+cout<<"the number a,b"<<largest<<endl;
+}

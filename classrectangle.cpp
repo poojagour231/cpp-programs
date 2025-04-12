18eaa7936f5d9f1f28652f2b9097c74e189bf715
Pooja gour <poojagour231@gmail.com>
2025-04-12 10:15:46 +0530

    Add files via upload

---
+++ classrectangle.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Rectangle{
+    private:
+    int length;
+    int width;
+    public:
+    Rectangle(int l,int w){
+        length=l;
+        width=w;
+    }
+    void display(){
+        cout<<"the length is"<<length<<endl;
+        cout<<"the width is"<<width<<endl;
+
+    }
+    int calculateArea(){
+        return length*width;
+    }
+    int calculatePerimeter(){
+        return 2*(length+width);
+    }
+};
+int main(){
+    Rectangle R1(3,4);
+    R1.display();
+    cout<<R1.calculateArea()<<endl;
+    cout<<R1.calculatePerimeter()<<endl;
+    return 0;
+}

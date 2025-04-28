e190c316989c68ca89cc735343943a898b26ab49
Pooja gour <poojagour231@gmail.com>
2025-04-28 18:04:47 +0530

    Add files via upload

---
+++ areafriendfun.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Circle{
+    private:
+    float pi;
+    int radius;
+    public:
+    Circle(float p=0,int r=0){
+        pi=p;
+        radius=r;
+    }
+    friend int CalculateArea(Circle c1);
+
+};
+int CalculateArea(Circle c1){
+   return c1.pi*c1.radius*c1.radius;
+}
+int main(){
+     Circle c1(3.14,5);
+     int totalArea=CalculateArea(c1);
+     cout<<"area of circle is"<<totalArea<<endl;
+}

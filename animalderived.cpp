f078812f46a934100b73b40993010c6984f9cc28
Pooja gour <poojagour231@gmail.com>
2025-04-28 18:04:13 +0530

    Add files via upload

---
+++ animalderived.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Animal{
+    public:
+    void eat (){
+        cout<<"eating"<<endl;
+
+
+    }
+};
+class Dog:public Animal{
+    public:
+    void bark(){
+        cout<<"barking"<<endl;
+    }
+
+
+};
+class Cat:public Animal{
+    public:
+    void meow(){
+        cout<<"meow"<<endl;
+    }
+};
+class Bird:public Animal{
+    public:
+    void chichi(){
+        cout<<"chichi"<<endl;
+    }
+};
+int main(){
+    Bird b1;
+    b1.eat();
+    b1.chichi();
+    return 0;
+
+
+}

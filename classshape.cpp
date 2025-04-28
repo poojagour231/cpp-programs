fb0cab9ab9e7b27cef568e7dbbd1f68a11123ee8
Pooja gour <poojagour231@gmail.com>
2025-04-28 21:29:54 +0530

    Add files via upload

---
+++ classshape.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Shape{
+    public:
+    void display() {
+        cout<<"this is the shape"<<endl;
+    }
+};
+class Circle:public Shape{
+    public:
+    int radius;
+    Circle(){
+        cout<<"this is the circle"<<endl;
+    }
+
+};
+class Rectangle:public Shape{
+    public:
+    int length;
+    int breath;
+    Rectangle(){
+        cout<<"this is the Rectangle"<<endl;
+    }
+};
+class Triangle:public Shape{
+    public:
+    Triangle(){
+        cout<<"this is the triangle"<<endl;
+    }
+};   
+int main(){
+    Triangle t1;
+    t1.display();
+    
+}

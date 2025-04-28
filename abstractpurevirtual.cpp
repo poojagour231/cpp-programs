8c89fdd10f4861b0ef2ac657fd0fe8f74b12242d
Pooja gour <poojagour231@gmail.com>
2025-04-28 18:01:02 +0530

    Add files via upload

---
+++ abstractpurevirtual.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Payment{
+    public:
+    virtual void processPayment(double amount)=0;   
+};
+class CreditcardPayment:public Payment{
+    public:
+    void processPayment(double amount)override{
+       cout<<"processPayment"<<amount<<endl;
+       cout<<"creditcardpayment"<<endl;
+    }
+};
+class DebitcardPayment:public Payment{
+    public:
+    void processPayment(double amount)override{
+        cout<<"processPayment"<<amount<<endl;
+        cout<<"debitcardpayment"<<endl;
+    }
+};
+int main(){
+    Payment*p;
+    CreditcardPayment credit;
+    DebitcardPayment debit;
+    p=&credit;
+    p->processPayment(20000);
+    p=&debit;
+    p->processPayment(300000);
+}

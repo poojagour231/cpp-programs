4ee9e9a40450ef76b70e33bc36b44fcdecc4aedd
Pooja gour <poojagour231@gmail.com>
2025-05-07 13:41:10 +0530

    Add files via upload

---
+++ cout_cin.cpp
@@ -0,0 +1,10 @@
+// Write a program to perform basic input and output using streams (cin and cout).
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number: " << endl;
+    cin >> n;
+    return 0;
+}

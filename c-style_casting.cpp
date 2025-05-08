fc7f9f6537fcf3bea7e16940c0a71a2e0ae13513
Pooja gour <poojagour231@gmail.com>
2025-05-08 13:11:43 +0530

    Add files via upload

---
+++ c-style_casting.cpp
@@ -0,0 +1,10 @@
+//25.	Write a C++ program to demonstrate the use of C-style casting by converting an int to a double.
+#include<iostream>
+using namespace std;
+int main(){
+    int num=45;
+    double doubleval=(double)num;
+    cout<<"int value:"<<num<<endl;
+    cout<<"double convert value:"<<doubleval<<endl;
+    return 0;
+}
